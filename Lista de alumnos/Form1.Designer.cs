d66045c0c4f72009ab7e0f093d37d2af51d3ffe8
JoelTroc <50915023+JoelTroc@users.noreply.github.com>
2022-04-06 20:45:37 -0300

    Add files via upload

---
+++ Lista de alumnos/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace Lista_de_alumnos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.t2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.t3 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.CODIGO = new System.Windows.Forms.ColumnHeader();
+            this.TEXTO = new System.Windows.Forms.ColumnHeader();
+            this.APELLIDO = new System.Windows.Forms.ColumnHeader();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(161, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // t1
+            // 
+            this.t1.Location = new System.Drawing.Point(231, 68);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(161, 23);
+            this.t1.TabIndex = 1;
+            this.t1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.Location = new System.Drawing.Point(161, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre";
+            // 
+            // t2
+            // 
+            this.t2.Location = new System.Drawing.Point(231, 128);
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(161, 23);
+            this.t2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(161, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Apellido";
+            // 
+            // t3
+            // 
+            this.t3.Location = new System.Drawing.Point(231, 211);
+            this.t3.Name = "t3";
+            this.t3.Size = new System.Drawing.Size(161, 23);
+            this.t3.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.CODIGO,
+            this.TEXTO,
+            this.APELLIDO});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(59, 273);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(407, 155);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // CODIGO
+            // 
+            this.CODIGO.Text = "CODIGO";
+            // 
+            // TEXTO
+            // 
+            this.TEXTO.Text = "TEXTO";
+            // 
+            // APELLIDO
+            // 
+            this.APELLIDO.Text = "APELLIDO";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(517, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 73);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "GUARDAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(573, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 51);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "SALIR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(530, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(517, 228);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 32);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "BORRAR TODO";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(492, 303);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(708, 465);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.t3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "LISTA DE ALUMNOS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox t2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox t3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader CODIGO;
+        private System.Windows.Forms.ColumnHeader TEXTO;
+        private System.Windows.Forms.ColumnHeader APELLIDO;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Lista de alumnos/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista_de_alumnos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListViewItem LISTA = new ListViewItem(t1.Text); //instancia al list view Y AGREGA EL TEXTO
+            LISTA.SubItems.Add(t2.Text); // AGREGA EL TEXTO AL LITS VIEW
+            LISTA.SubItems.Add(t3.Text); // AGREGA EL TEXTO AL LIST VIEW
+            listView1.Items.Add(LISTA);
+            t1.Text = ""; //BORRA EL TEXTO DEL TEXT BOX CUADNO APRIETO EN EL BOTON GUARDAR
+            t2.Text = ""; //BORRA EL TEXTO DEL TEXT BOX CUANDO APRIETO EN EL BOTON GUARDAR
+            t3.Text = ""; //BORRA EL TEXTO DEL TEXT BOX CUANDO APRIETO EN EL BOTON GUARDAR
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close(); // CIERRA EL PROGRAMA CUANDO APRIETO EN EL BOTON SALIR
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            listView1.Items.Add(label4.Text);
+              
+
+
+        }
+
+        private void listView1_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+          
+            label4.Text = listView1.SelectedItems.ToString();
+
+        }
+        //SACAR DATOS DEL LIST VIWE Y MOVERLOS A UN LABEL
+    }
+}
